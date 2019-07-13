c8302cbde08549da3e33712e90b41b04030666ad
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-07-13 23:57:05 +0200

    add dynamic form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './ninjas';
+import AddNinja from './AddNinja'
 
 
 class App extends Component {
@@ -9,9 +10,9 @@ class App extends Component {
       { name: "Mietek", age: '99', belt: 'black', key: 1 },
       { name: "Kazik", age: '98', belt: 'red', key: 2 },
       { name: "Bolek", age: '101', belt: 'green', key: 3 },
-      { name: "Adam", age: '19', belt: 'green', key: 3 },
-      { name: "Ola", age: '15', belt: 'green', key: 3 },
-      { name: "Kasia", age: '10', belt: 'green', key: 3 }
+      { name: "Adam", age: '19', belt: 'green', key: 4 },
+      { name: "Ola", age: '15', belt: 'green', key: 5 },
+      { name: "Kasia", age: '10', belt: 'green', key: 6 }
     ]
   }
   render() {
@@ -20,6 +21,7 @@ class App extends Component {
         <h1>My first React App</h1>
         <p>Welcome :-)</p>
         <Ninjas ninjas={this.state.ninjas} />
+        <AddNinja />
 
       </div>
     );
